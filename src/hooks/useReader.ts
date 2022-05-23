0bbb9703aa1bb1044fe7abaa679e4a47d8c72eee
Noman989 <nauman@authornate.com>
2022-05-23 16:10:48 +0500

    updated useReader hook

--- src/hooks/useReader.ts
+++ src/hooks/useReader.ts
@@ -3,73 +3,82 @@ import { ReaderContext, ReaderContextProps } from '../context';
 
 export function useReader() {
   const {
-    changeFontSize,
-    changeFontFamily,
-    changeTheme,
-    goToLocation,
-    goPrevious,
-    goNext,
-    getLocations,
-    getCurrentLocation,
-    search,
-    addMark,
-    removeMark,
-    theme,
-    atStart,
-    atEnd,
-    totalLocations,
-    currentLocation,
-    progress,
-    locations,
-    isLoading,
-    key,
-    searchResults,
+    view1,
+    view2,
+    view3,
+    // changeFontSize,
+    // changeFontFamily,
+    // changeTheme,
+    // goToLocation,
+    // goPrevious,
+    // goNext,
+    // getLocations,
+    // getCurrentLocation,
+    // search,
+    // addMark,
+    // removeMark,
+    // theme,
+    // atStart,
+    // atEnd,
+    // totalLocations,
+    // currentLocation,
+    // progress,
+    // locations,
+    // isLoading,
+    // key,
+    // searchResults,
   } = useContext(ReaderContext);
 
   return {
-    changeFontSize,
-    changeFontFamily,
-    changeTheme,
-    goToLocation,
-    goPrevious,
-    goNext,
-    getLocations,
-    getCurrentLocation,
-    search,
-    addMark,
-    removeMark,
-    theme,
-    atStart,
-    atEnd,
-    totalLocations,
-    currentLocation,
-    progress,
-    locations,
-    isLoading,
-    key,
-    searchResults,
+    view1,
+    view2,
+    view3,
+    // changeFontSize,
+    // changeFontFamily,
+    // changeTheme,
+    // goToLocation,
+    // goPrevious,
+    // goNext,
+    // getLocations,
+    // getCurrentLocation,
+    // search,
+    // addMark,
+    // removeMark,
+    // theme,
+    // atStart,
+    // atEnd,
+    // totalLocations,
+    // currentLocation,
+    // progress,
+    // locations,
+    // isLoading,
+    // key,
+    // searchResults,
   } as Pick<
     ReaderContextProps,
-    | 'changeFontSize'
-    | 'changeFontFamily'
-    | 'changeTheme'
-    | 'goToLocation'
-    | 'goPrevious'
-    | 'goNext'
-    | 'getLocations'
-    | 'getCurrentLocation'
-    | 'search'
-    | 'addMark'
-    | 'removeMark'
-    | 'theme'
-    | 'atStart'
-    | 'atEnd'
-    | 'totalLocations'
-    | 'currentLocation'
-    | 'progress'
-    | 'locations'
-    | 'isLoading'
-    | 'key'
-    | 'searchResults'
+    | 'view1'
+    | 'view2'
+    | 'view3'
+    // | 'changeFontSize'
+    // | 'changeFontFamily'
+    // | 'changeTheme'
+    // | 'goToLocation'
+    // | 'goPrevious'
+    // | 'goNext'
+    // | 'getLocations'
+    // | 'getCurrentLocation'
+    // | 'search'
+    // | 'addMark'
+    // | 'removeMark'
+    // | 'theme'
+    // | 'atStart'
+    // | 'atEnd'
+    // | 'totalLocations'
+    // | 'currentLocation'
+    // | 'progress'
+    // | 'locations'
+    // | 'isLoading'
+    // | 'key'
+    // | 'searchResults'
   >;
 }
